79796aeb494d5da42890b53936fe8d3bc6452324
M0T3L <yavuzburakkutluca@hotmail.com>
2026-08-13 10:47:01 +0300

    fix: remove unused StaticFS embed in web/embed.go to resolve build error

--- web/embed.go
+++ web/embed.go
@@ -7,7 +7,3 @@ import (
 // TemplateFS embeds all HTML templates.
 //go:embed templates/*.html templates/partials/*.html
 var TemplateFS embed.FS
-
-// StaticFS embeds static asset files if present.
-//go:embed static
-var StaticFS embed.FS
